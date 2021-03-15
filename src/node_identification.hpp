5845c9f29e2533662b31c85de3204da6dd1dcacd
devyanijivani <devyanigjivani@gmail.com>
2021-03-15 10:55:07 -0400

    completed node identification and added examples

--- src/node_identification.hpp
+++ src/node_identification.hpp
@@ -15,6 +15,8 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <algorithm> // for std::find
+#include <iterator> // for std::begin, std::end
 
 
 namespace graspi {
@@ -37,7 +39,7 @@ typedef struct{
 /// @param thispixel  is the pixel for which the neighbor data is to be calculated
 /// @param skeleton are the pixel positions corresponding to the skeleton
 /// @param skelsize is the length of the skeleton
-/// @return neighbordata the information about the 8-neighborhood of the input pixel
+/// @return struct encoding the information about the 8-neighborhood of the input pixel
 
 neighbor* getNeighbors(node* thispixel, node* skeleton, int skelsize){ //Number of neighbors that belong to the image
     neighbor* neighbordata = new neighbor [9];
@@ -48,10 +50,13 @@ neighbor* getNeighbors(node* thispixel, node* skeleton, int skelsize){ //Number 
     neighbordata->even = 0;
     neighbordata->odd = 0;
     for(int i = 0; i < 8; i++){
+//        std::cout << neighbors[i].row << " , " << neighbors[i].col << " --- \n";
         neighbordata->direction[i] = 0;
         for(int j=0; j < skelsize; j++){
+//            std::cout << skeleton[j].row << " , " << skeleton[j].col << "\n";
             if (neighbors[i].row == skeleton[j].row && neighbors[i].col == skeleton[j].col){
                 neighbordata->direction[i] = 1;
+//                std::cout << skeleton[j].row << " , " << skeleton[j].col << "-------" << neighbors[i].row << " , " << neighbors[i].col << "\n";
                 neighbordata->neighbors[neighbordata->count].row = neighbors[i].row;
                 neighbordata->neighbors[neighbordata->count].col = neighbors[i].col;
                 neighbordata->count++ ;
@@ -67,7 +72,7 @@ neighbor* getNeighbors(node* thispixel, node* skeleton, int skelsize){ //Number 
             }
         }
     }
-    std::cout << neighbordata->count++;
+//    std::cout << neighbordata->count;
     return neighbordata;
 }
 
@@ -92,6 +97,7 @@ bool getNeighborPosition(neighbor* neighbordata, int* ndir, int dir_count){
     return neighbor_exists;
 }
 
+
 /// This function deletes the elements of struct Node
 /// @param delposition  is the index to the deleted
 /// @param allNodes is the struct from which an entry is to be deleted
@@ -108,6 +114,20 @@ node* delNode(int delposition, node* allNodes, int lenAllNodes) {
     return allNodes;
 }
 
+/// This function return true if the element is in the array
+/// @param val  is the element
+/// @param inputArray is the array
+/// @param sizeA is the size of inputArray
+/// @return true if the element is in inputArray
+
+bool isInArray(int val, int* inputArray, int sizeA){
+    for(int i = 0; i < sizeA; i++) {
+        if (inputArray[i] == val)
+            return true;
+    }
+    return false;
+}
+
 /// This function computes skeletal descriptors of morphology defined on structured matrix
 ///
 /// This function computes a set of descriptors of morphology
@@ -117,21 +137,23 @@ node* delNode(int delposition, node* allNodes, int lenAllNodes) {
 /// @param phasePixel is the value of the pixel that corresponds to the skeleton in skeletonMatrix
 /// @return the vector of descriptors, where each descriptor is a pair (std::pair<float,std::string>) consisting of descriptor's value (float) and name (string)
 
-int identifyIntersections(int** skeletonMatrix, int nx, int ny, int phasePixel) {
+int* identifyIntersections(int** skeletonMatrix, int nx, int ny, int phasePixel) {
     
-    node* skeleton;
+    node* skeleton = new node[(ny+1)*(nx+1)];
 
-    skeleton = (node*)malloc((ny+1)*(nx+1)*sizeof(node));
     int skelpixel = 0;
-    for(int i=1;i<ny;i++) {
-        for(int j=1;j<=nx;j++) {
+    for(int i=0;i<ny;i++) {
+        for(int j=0;j<nx;j++) {
             if(skeletonMatrix[i][j]==phasePixel) {
                 skeleton[skelpixel].row = i;
                 skeleton[skelpixel].col = j;
                 skelpixel++;
             }
+            std::cout << skeletonMatrix[i][j] << " ";
         }
+        std::cout <<  "\n";
     }
+
  
     neighbor* thispixel_neighbors = new neighbor;
     node* thispixel = new node;
@@ -142,8 +164,8 @@ int identifyIntersections(int** skeletonMatrix, int nx, int ny, int phasePixel) 
     int endcount = 0, potjunctcount = 0;
     for(int i = 0; i < skelpixel; i++) {
         thispixel = &skeleton[i];
-        std::cout << thispixel->row << " , " << thispixel->col << "\n";
-        thispixel_neighbors = getNeighbors(thispixel, skeleton, skelpixel - 1);
+//        std::cout << thispixel->row << " , " << thispixel->col << "\n";
+        thispixel_neighbors = getNeighbors(thispixel, skeleton, skelpixel);
         
         if(thispixel_neighbors->count == 1) {
             branchEnd[endcount] = *thispixel;
@@ -154,26 +176,37 @@ int identifyIntersections(int** skeletonMatrix, int nx, int ny, int phasePixel) 
             potjunctcount++;
         }
         else if(thispixel_neighbors->count == 2) {
-            std::cout << "Do something! \n";
+//            std::cout << "Do something! \n";
         }
     }
+    
+    
+    // DEBUG
+//    for(int i = 0 ; i < potjunctcount; i++){
+//        std::cout <<  potentialJunction[i].row << " , " << potentialJunction[i].col << "\n";
+//    }
+//    for(int i = 0 ; i < endcount; i++){
+//        std::cout <<  branchEnd[i].row << " , " << branchEnd[i].col << "\n";
+//    }
 
     // Identify junction nodes from potential junction nodes
     node* delpixel = new node[skelpixel];
+    int* delIdx = new int[potjunctcount];
     int del = 0;
     for(int i = 0; i < potjunctcount; i++) {
         node* curpixel = new node;
         curpixel = &potentialJunction[i];
         neighbor* potJunc_neighbors = new neighbor;
         neighbor* first_neighbors = new neighbor;
-        first_neighbors = getNeighbors(curpixel, skeleton, skelpixel - 1);
+        first_neighbors = getNeighbors(curpixel, skeleton, skelpixel);
         potJunc_neighbors = getNeighbors(curpixel, potentialJunction, potjunctcount);
         bool delflag = 0;
         for (int j = 0; j < endcount; j++){
             for (int k = 0; k < first_neighbors->count; k++) {
                 if (branchEnd[j].row == first_neighbors->neighbors[k].row && branchEnd[j].col == first_neighbors->neighbors[k].col) {
-                    delpixel[del].row = curpixel->row;
-                    delpixel[del].col = curpixel->col;
+ //                   delpixel[del].row = curpixel->row;
+ //                   delpixel[del].col = curpixel->col;
+                    delIdx[del] = i;
                     del++;
                     delflag = 1;
                     continue;
@@ -186,11 +219,12 @@ int identifyIntersections(int** skeletonMatrix, int nx, int ny, int phasePixel) 
             }
             
         }
-        if (potJunc_neighbors->count > 0 && first_neighbors->odd == 2) {
-            if ((first_neighbors->direction[0] == 1 && first_neighbors->direction[4] == 1) ||
-                (first_neighbors->direction[2] == 1 && first_neighbors->direction[6] == 1)) {
+        if (potJunc_neighbors->count > 0 && first_neighbors->even == 2) {
+            if ((first_neighbors->direction[0] == true && first_neighbors->direction[4] == true) ||
+                (first_neighbors->direction[2] == true && first_neighbors->direction[6] == true)) {
                 delpixel[del].row = curpixel->row;
                 delpixel[del].col = curpixel->col;
+                delIdx[del] = i;
                 del++;
                 delflag = 1;
                 continue;
@@ -205,45 +239,117 @@ int identifyIntersections(int** skeletonMatrix, int nx, int ny, int phasePixel) 
         delete first_neighbors;
         delete potJunc_neighbors;
     }
-    int lenAllPot = potjunctcount;
+//    std::cout << del;
     node* updatedpotentialJunction = new node[potjunctcount];
-    for (int d = 0; d < del; d++) {
+    
+    int tlen = 0;
+    
+    // Deleting marked pixels
+    
+    if(del == 0) {
+        updatedpotentialJunction = potentialJunction;
+        tlen = potjunctcount;
+    }
+    else {
         for (int i = 0; i < potjunctcount; i++) {
-            if (potentialJunction[i].row == delpixel[i].row &&
-                potentialJunction[i].col == delpixel[i].col) {
-                updatedpotentialJunction = delNode(i, potentialJunction, lenAllPot);
-                lenAllPot--;
+            if (isInArray(i, delIdx, del)) {
+                continue;
+            }
+            else {
+                updatedpotentialJunction[tlen].row = potentialJunction[i].row;
+                updatedpotentialJunction[tlen].col = potentialJunction[i].col;
+                tlen++;
             }
         }
     }
     
+    
     delete [] delpixel;
     delete [] potentialJunction;
     
-    for(int i = 0; i < lenAllPot; i++) {
+    del = 0;
+    
+    for(int i = 0; i < tlen; i++) {
         
         node* curpixel = new node;
         curpixel = &updatedpotentialJunction[i];
         neighbor* potJunc_neighbors = new neighbor;
         neighbor* first_neighbors = new neighbor;
-        first_neighbors = getNeighbors(curpixel, skeleton, skelpixel - 1);
-        potJunc_neighbors = getNeighbors(curpixel, potentialJunction, potjunctcount);
-        node* delpixel = new node[skelpixel];
-        int* odd_second_neighbor_count = new int[potJunc_neighbors->count];
+        first_neighbors = getNeighbors(curpixel, skeleton, skelpixel);
+        potJunc_neighbors = getNeighbors(curpixel, updatedpotentialJunction, tlen);
+        int tempsize = potJunc_neighbors->count + 1;
+        int* even_neighbor_count = new int[tempsize];
+        even_neighbor_count[0] = first_neighbors->even;
+        node* temp_node = new node[tempsize];
+        temp_node[0].row = curpixel->row;
+        temp_node[0].col = curpixel->col;
         if (potJunc_neighbors->count > 0) {
             for(int j = 0; j < potJunc_neighbors->count; j++) {
                 node* curneighbor = new node;
                 curneighbor = &potJunc_neighbors->neighbors[j];
                 neighbor* second_neighbors = new neighbor;
-                second_neighbors = getNeighbors(curneighbor, skeleton, skelpixel - 1);
-                odd_second_neighbor_count = &second_neighbors->odd;
+                second_neighbors = getNeighbors(curneighbor, skeleton, skelpixel);
+                even_neighbor_count[j+1] = second_neighbors->even;
+                temp_node[j+1].row = curneighbor->row;
+                temp_node[j+1].col = curneighbor->col;
             }
-            
+            int maximumIdx = 0;
+            int max = even_neighbor_count[0];
+            for (int d = 0; d < tempsize; d++){
+                if (even_neighbor_count[d] > max){
+                        max = even_neighbor_count[d];
+                        maximumIdx = d;
+                }
+            }
+            delpixel[del].row = temp_node[maximumIdx].row;
+            delpixel[del].col = temp_node[maximumIdx].col;
+            del++;
         }
         
         
     }
-    return 0;
+    
+    node* branchJunction = new node[tlen];
+    
+    int junctcount = 0;
+    
+    // Deleting marked pixels
+    int* removeIdx  = new int[tlen];
+    int r = -1;
+    if(del == 0) {
+        branchJunction = updatedpotentialJunction;
+        junctcount = tlen;
+    }
+    else {
+        for (int i = 0; i < tlen; i++) {
+            for(int j = 0 ; j < del; j++) {
+                if(updatedpotentialJunction[i].row == delpixel[j].row && updatedpotentialJunction[i].col == delpixel[j].col) {
+                    r++;
+                    removeIdx[r] = i;
+                    continue;
+                }
+            }
+        }
+    }
+    
+    for (int rem = 0; rem <= r; rem++) {
+        if(isInArray(rem, removeIdx, r+1)){
+            continue;
+        }
+        else {
+            branchJunction[junctcount].row = updatedpotentialJunction[rem].row;
+            branchJunction[junctcount].col = updatedpotentialJunction[rem].col;
+            std::cout << branchJunction[junctcount].row << " , " << branchJunction[junctcount].col << "\n";
+            junctcount++;
+        }
+        
+    }
+    
+    int* node_counts = new int[2];
+    node_counts[0] = endcount - 1;
+    node_counts[1] = junctcount - 1;
+    return node_counts;
 }
 }
+
 #endif /* node_identification_h */
